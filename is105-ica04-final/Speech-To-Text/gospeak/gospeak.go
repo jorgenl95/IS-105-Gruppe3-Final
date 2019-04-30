b00212cd0bb664f5bc1fc35932f6f89d1e3f1eb8
Hattemaker <46780328+Hattemaker@users.noreply.github.com>
2019-04-30 17:22:41 +0200

    Update gospeak.go

--- is105-ica04-final/Speech-To-Text/gospeak/gospeak.go
+++ is105-ica04-final/Speech-To-Text/gospeak/gospeak.go
@@ -5,7 +5,7 @@ import (speech "github.com/nicolaifsf/go-speak")
 
 
 func main(){
-  speech.SetWitKey("RLD2DHGWIRNX72FHOZMRNBTGJBGOCEXK") //Wit API Key MUST be set before calling any other Wit.AI functions
+  speech.SetWitKey("RLD2DHGWIRNX72FHOZMRNBTGJBGOCEXK") //Wit API key fra sveggen
   speech.SendWitVoice("audio-file.wav")
   
 
